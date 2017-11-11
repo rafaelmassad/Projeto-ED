0f593e5a34b002b1d4c3e9a8c18af0da67149e45
LuisMiara <luisgustavomiara@gmail.com>
2017-11-11 10:42:03 -0200

    commitando loucamente

--- Projeto ED/main.c
+++ Projeto ED/main.c
@@ -1,48 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
 typedef struct lista{
-    int info;
+    int peso;
+    int raridade;
+    int subtracaoRaridadePeso;
     struct lista* prox;
-}Lista;
-void imprimi(Lista* A){
-    while(A!=NULL){
-        printf("%d",A->info);
-        A=A->prox;
+    struct lista* ant;
+}ListaDupla;
+
+
+
+void imprimi(ListaDupla* L){
+    while(L!=NULL){
+        printf("Peso = %d |\t",L->peso);
+        printf("Raridade = %d\t",L->raridade);
+        printf("SubtracaoRaridadePeso = %d\n",L->subtracaoRaridadePeso);
+        
+        L=L->prox;
     }
 }
-Lista* insere_Inicio_Lista (Lista* A, int valor){
-    Lista* novo = (Lista*)malloc(sizeof(Lista));
-
-    novo->info = valor;
-    novo->prox = NULL;
-
-    return novo;
+//funcao para popular a lista com o valor de peso e raridade do dataset
+ListaDupla* insereInicio(ListaDupla* L, int peso, int raridade){
+	ListaDupla* novo = (ListaDupla*) malloc(sizeof(ListaDupla));
+	novo->peso = peso;
+	novo->raridade = raridade;
+	novo->prox = L;
+	novo->ant = NULL;
+	if(L == NULL){
+		return novo;
+	}else{
+		L->ant = novo;
+		return novo;
+	}
 }
-Lista* insere_Fim_Lista(Lista *L, int valor){
-    Lista *novo = (Lista*)malloc(sizeof(Lista));
-    Lista* aux = L;
 
-    novo->info = valor;
-    novo->prox = NULL;
-
-    if(L == NULL){
-        return novo;
-    }else{
-        while(aux->prox !=NULL){
-            aux = aux->prox;
-        }
-        aux->prox = novo;
-        return L;
-    }
+ListaDupla* insereFim(ListaDupla* L,int peso, int raridade,int resultado){
+	ListaDupla* novo = (ListaDupla*) malloc(sizeof(ListaDupla));
+	ListaDupla* aux = (ListaDupla*) malloc(sizeof(ListaDupla));
+	novo->ant = NULL;
+	novo->peso = peso;
+	novo->raridade = raridade;
+	novo->subtracaoRaridadePeso = resultado;
+	novo->prox = NULL;
+	aux->prox = L;
+	
+	if(L == NULL){
+		return novo;
+	}
+	
+	while(aux->prox != NULL){
+		aux=aux->prox;
+	}
+	
+	aux->prox = novo;
+	novo->ant = aux;
+	
+	return L;
 }
+
+
+
+
+
 int main()
 {
-Lista* peso = (Lista*)malloc(sizeof(Lista));
-Lista* raridade = (Lista*)malloc(sizeof(Lista));
+ListaDupla* mochila = (ListaDupla*)malloc(sizeof(ListaDupla));
 
 FILE *arq, *arq2, *arq3;
 char str[10], str2[50], str3[50];
 char *result;
+char *result1;
+char *result2;
+int peso = NULL;
+ListaDupla* L = NULL;
 
 int capacidade;
 int i;
@@ -63,33 +95,30 @@ if(arq3==NULL){
 }
 //----------------------------Leitura do Arquivo ---------------------------
 while (!feof(arq)){
+	
 	// Lê uma linha (inclusive com o '\n')
       result = fgets(str, 100, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
       if (result)  // Se foi possível ler
         peso = atoi(str);
 }
 while (!feof(arq2)){
+	int resultado = NULL;
 	// Lê uma linha (inclusive com o '\n')
-      result = fgets(str2, 100, arq2);  // o 'fgets' lê até 99 caracteres ou até o '\n'
-      if (result)  // Se foi possível ler
-        peso = insere_Fim_Lista(peso, atoi(str2));
+      result1 = fgets(str2, 100, arq2);
+	  result2 = fgets(str3, 100, arq3);  // o 'fgets' lê até 99 caracteres ou até o '\n'
+	  resultado =   atoi(str2) - atoi(str3);
+      if (result1)  // Se foi possível ler
+        L = insereFim(L, atoi(str2), atoi(str3), resultado);
 }
- while (!feof(arq3)){
-	// Lê uma linha (inclusive com o '\n')
-      result = fgets(str3, 100, arq3);  // o 'fgets' lê até 99 caracteres ou até o '\n'
-      if (result){  // Se foi possível ler
-        raridade = insere_Fim_Lista(raridade, atoi(str3));
-      }
-}
+
 //---------------------------Fecha os arquivoss ---------------------------------
 fclose(arq);
 fclose(arq2);
 fclose(arq3);
 
 //-------------------------impressao dps valores -------------------------------
-printf("Capacidade mochila 1: %d", peso);
-imprimi(peso);
-imprimi(raridade);
+printf("Capacidade mochila 1: %d\n", peso);
+imprimi(L);
 printf("Teste");
 }
 
