e6742322cc733f31250a97d0b88c0749eb83bf13
LuisMiara <luisgustavomiara@gmail.com>
2017-11-11 11:51:10 -0200

    quebra galho

--- Projeto ED/main.c
+++ Projeto ED/main.c
@@ -5,7 +5,7 @@
 typedef struct lista{
     int peso;
     int raridade;
-    int subtracao_Raridade_Peso;
+    int subtracaoRaridadePeso;
     struct lista* prox;
     struct lista* ant;
 }ListaDupla;
@@ -14,9 +14,9 @@ typedef struct lista{
 
 void imprimi(ListaDupla* L){
     while(L!=NULL){
-        printf("\nPeso = %d ",L->peso);
-        printf("\nRaridade = %d",L->raridade);
-        printf("\nSubtracao Raridade e Peso = %d",L->subtracao_Raridade_Peso);
+        printf("Peso = %d |\t",L->peso);
+        printf("Raridade = %d\t",L->raridade);
+        printf("SubtracaoRaridadePeso = %d\n",L->subtracaoRaridadePeso);
         
         L=L->prox;
     }
@@ -42,7 +42,7 @@ ListaDupla* insereFim(ListaDupla* L,int peso, int raridade,int resultado){
 	novo->ant = NULL;
 	novo->peso = peso;
 	novo->raridade = raridade;
-	novo->subtracao_Raridade_Peso = resultado;
+	novo->subtracaoRaridadePeso = resultado;
 	novo->prox = NULL;
 	aux->prox = L;
 	
@@ -60,6 +60,70 @@ ListaDupla* insereFim(ListaDupla* L,int peso, int raridade,int resultado){
 	return L;
 }
 
+ListaDupla* removeListaDupla(ListaDupla* L, int info){
+	printf("Removendo->%d...", info);
+	ListaDupla* aux;
+	ListaDupla* ant;
+	aux = L;
+	
+	if(aux == NULL){
+		return NULL;
+	}
+	
+	while(aux != NULL && aux->info != info){
+		ant = aux;
+		aux = aux->prox;
+	}
+	
+	if(aux->ant == NULL){
+		L = aux->prox;
+		aux->prox->ant = NULL;
+		free(aux);
+		return L;
+	}
+	if(aux->prox == NULL){
+		aux->ant->prox = NULL;
+		free(aux);
+		return L;
+	}
+	
+	aux->ant->prox = aux->prox;
+	aux->prox->ant = aux->ant;
+	
+	return L;
+}
+//ordena do maior para o menor de acordo dom o atributo subRaridadePeso
+ListaDupla* ordenaLista(ListaDupla* L){
+	ListaDupla* nLista = NULL;
+	ListaDupla* aux = NULL;
+	ListaDupla* aux1 = NULL;
+	ListaDupla* maior = NULL;
+	
+	aux1= L;
+	aux = L;
+	maior = L;
+	
+	while(aux1 != NULL){
+		aux = aux1;
+		while(aux != NULL && aux->subtracaoRaridadePeso > maior->subtracaoRaridadePeso){
+			maior = aux;
+			aux = aux->prox;
+		}
+
+		nLista = insereFim(nLista, maior->peso, maior->raridade, maior->subtracaoRaridadePeso);
+		free(maior);
+		aux1 = aux1->prox;
+	}	
+	
+	return nLista;	
+}
+
+//fazer funcao para inserir melhores itens na mochila, conforme o maior valor da subtracaoRaridadePeso
+ListaDupla* insereItensMochila(ListaDupla* L, int capacidade){
+	
+	return NULL;
+}
+
 
 
 
@@ -73,10 +137,9 @@ char str[10], str2[50], str3[50];
 char *result;
 char *result1;
 char *result2;
-int peso = NULL;
+int capacidade = NULL;
 ListaDupla* L = NULL;
 
-int capacidade;
 int i;
 
 arq = fopen("p01_c.txt", "rt");
@@ -99,7 +162,7 @@ while (!feof(arq)){
 	// Lê uma linha (inclusive com o '\n')
       result = fgets(str, 100, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
       if (result)  // Se foi possível ler
-        peso = atoi(str);
+        capacidade = atoi(str);
 }
 while (!feof(arq2)){
 	int resultado = NULL;
@@ -117,8 +180,11 @@ fclose(arq2);
 fclose(arq3);
 
 //-------------------------impressao dps valores -------------------------------
-printf("Capacidade mochila 1: %d\n", peso);
+printf("Capacidade mochila 1: %d\n", capacidade);
 imprimi(L);
-
+printf("\n\n");
+printf("Capacidade mochila 1: %d\n", capacidade);
+L = ordenaLista(L);
+imprimi(L);
+printf("Teste");
 }
-
